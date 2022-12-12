7b085ac93d0091126bfff2e2d7896f8f8a989d95
mithang <minhtv.1602@yahoo.com>
2022-12-12 21:40:18 +0700

    Initial commit

---
+++ Assets/Scripts/NhanVatController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NhanVatController : MonoBehaviour {
+
+    Animator animator;
+
+    // Use this for initialization
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //NhanVatState chính là tên mà đã đặt bên trong animator
+        if (Input.GetKey(KeyCode.A))
+        {
+            
+            animator.SetInteger("NhanVatState", 0);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            
+            animator.SetInteger("NhanVatState", 1);
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+
+            animator.SetInteger("NhanVatState", 2);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+
+            animator.SetInteger("NhanVatState", 3);
+        }
+    }
+}
---
+++ Assets/Scripts/PersonController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonController : MonoBehaviour {
+
+    Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //AnimaState chính là tên mà đã đặt bên trong animator
+        if (Input.GetKey(KeyCode.A))
+        {
+            //Chạy
+            animator.SetInteger("AnimaState",0);
+        }
+        else
+        {
+            //Đứng
+            animator.SetInteger("AnimaState",1);
+        }
+	}
+}
